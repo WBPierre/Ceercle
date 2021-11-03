b1b1821d748d35089f42c7f39ca1079c5c1044c6
DELMER <d.pierrebox@gmail.com>
2021-11-03 11:40:34 +0100

    Removing unused lib

--- frontend/src/components/molecules/home/TypingEffect.js
+++ frontend/src/components/molecules/home/TypingEffect.js
@@ -1,5 +1,4 @@
 import {useEffect, useState} from "react";
-import TypeWriterEffect from "react-typewriter-effect";
 
 const words = ["dans vos bureaux.", "en télétravail.", "dans des tiers-lieux."];
 
