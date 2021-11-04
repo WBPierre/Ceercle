1bfcf7ceeb4d814648b937add79ac5c0c22c12f9
DELMER <d.pierrebox@gmail.com>
2021-11-04 17:50:53 +0100

    Adding images to SC value

--- frontend/src/components/containers/home/SCValueList.js
+++ frontend/src/components/containers/home/SCValueList.js
@@ -6,6 +6,11 @@ import GridModule from "../../containers/home/GridModule";
 import SCValueElement from "../../molecules/home/SCValueElement";
 import {useTheme} from "@mui/material";
 import Container from "@mui/material/Container";
+import brand from "../../../assets/images/home/value/brand.png";
+import culture from "../../../assets/images/home/value/culture.png";
+import eco from "../../../assets/images/home/value/eco.png";
+import liberty from "../../../assets/images/home/value/liberty.png";
+
 
 function SCValueList(props){
     
@@ -29,11 +34,10 @@ function SCValueList(props){
                     </Grid>
                     <Grid item>
                         <Grid container direction="row" spacing={3} justifyContent="center">
-                            <SCValueElement title={ t("home:SCValue.sub_element_1_title") } text={ t("home:SCValue.sub_element_1_content") }/>
-                            <SCValueElement title={ t("home:SCValue.sub_element_2_title") } text={ t("home:SCValue.sub_element_2_content") }/>
-                            <SCValueElement title={ t("home:SCValue.sub_element_3_title") } text={ t("home:SCValue.sub_element_3_content") }/>
-                            <SCValueElement title={ t("home:SCValue.sub_element_4_title") } text={ t("home:SCValue.sub_element_4_content") }/>
-
+                            <SCValueElement title={ t("home:SCValue.sub_element_1_title") } image={brand} text={ t("home:SCValue.sub_element_1_content") }/>
+                            <SCValueElement title={ t("home:SCValue.sub_element_2_title") } image={culture} text={ t("home:SCValue.sub_element_2_content") }/>
+                            <SCValueElement title={ t("home:SCValue.sub_element_3_title") } image={eco} text={ t("home:SCValue.sub_element_3_content") }/>
+                            <SCValueElement title={ t("home:SCValue.sub_element_4_title") } image={liberty} text={ t("home:SCValue.sub_element_4_content") }/>
                         </Grid>
                     </Grid>
                 </GridModule>
--- frontend/src/components/molecules/home/SCValueElement.js
+++ frontend/src/components/molecules/home/SCValueElement.js
@@ -2,13 +2,15 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import FreeBreakfastIcon from '@mui/icons-material/FreeBreakfast';
 import GridModule from "../../containers/home/GridModule";
+import Paper from "@mui/material/Paper";
+import Essec from "../../../assets/images/footer/essec.jpg";
 
 function SCValueElement(props){
     return(  
             <Grid item xs={12} md={3} align="center">
                 <Grid container direction="column" >
                     <Grid item>
-                        <FreeBreakfastIcon/>
+                        <img src={props.image} alt="value image" style={{height: '20vmin', width:'100%'}}/>
                     </Grid>
                     <Grid item>
                         <Typography variant="subtitle1" fontWeight={600}>
