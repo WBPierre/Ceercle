8a22bc887838c374c7c3b7b326ef87b5e3710a4c
DELMER <d.pierrebox@gmail.com>
2021-11-11 19:11:28 +0100

    Adding Offers navigation

--- frontend/src/navigation/Rooter.js
+++ frontend/src/navigation/Rooter.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import * as Public_Routes from "./public/Routes";
 import Home from "../views/Home";
+import Offers from "../views/Offers";
 
 
 function Rooter(){
@@ -8,6 +9,7 @@ function Rooter(){
         <Router>
             <Routes>
                 <Route exact path={Public_Routes.HOME} element={<Home/>} />
+                <Route exact path={Public_Routes.OFFERS} element={<Offers/>}/>
             </Routes>
         </Router>
     )
---
+++ frontend/src/views/Offers.js
@@ -0,0 +1,33 @@
+import logo from './../logo.svg';
+import {useTranslation} from "react-i18next";
+import Button from '@mui/material/Button';
+import NavBar from "./../components/containers/navbar/NavBar"
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography"
+import HomeImage from "../assets/images/home/homeIntro.png"
+import Box from "@mui/material/Box";
+import DescriptionList from "../components/containers/home/DescriptionList";
+import {useEffect, useRef, useState} from "react";
+import Divider from "@mui/material/Divider";
+import SCValueList from "../components/containers/home/SCValueList";
+import Demo from "../components/molecules/home/Demo";
+import UserInterfaceList from "../components/containers/home/UserInterfaceList";
+import Footer from "../components/containers/footer/Footer";
+import TypingEffect from "../components/molecules/home/TypingEffect";
+import {useTheme} from "@mui/material";
+
+function Offers(){
+    const { t } = useTranslation();
+    const theme = useTheme();
+
+    return(
+        <Container maxWidth={false} disableGutters={true}>
+            <NavBar/>
+            <Demo/>
+            <Footer/>
+        </Container>
+    )
+}
+
+export default Offers;
