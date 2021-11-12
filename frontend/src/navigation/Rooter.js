47cf0d020061dbfc41e4e31f691f41daae9ac13f
DELMER <d.pierrebox@gmail.com>
2021-11-12 17:26:36 +0100

    Adding contact views

--- frontend/src/navigation/Rooter.js
+++ frontend/src/navigation/Rooter.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import * as Public_Routes from "./public/Routes";
 import Home from "../views/Home";
 import Offers from "../views/Offers";
+import Contact from "../views/Contact";
 
 
 function Rooter(){
@@ -10,6 +11,7 @@ function Rooter(){
             <Routes>
                 <Route exact path={Public_Routes.HOME} element={<Home/>} />
                 <Route exact path={Public_Routes.OFFERS} element={<Offers/>}/>
+                <Route exact path={Public_Routes.CONTACT} element={<Contact/>} />
             </Routes>
         </Router>
     )
