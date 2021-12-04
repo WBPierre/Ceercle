48a24daa55d3a302f98a67651733ce2802d7d11c
DELMER <d.pierrebox@gmail.com>
2021-12-04 19:04:37 +0100

    Addin scrollToTop whenever page change

--- frontend/src/navigation/Rooter.js
+++ frontend/src/navigation/Rooter.js
@@ -2,13 +2,16 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import PublicRooter from "./public/Router";
 import AdminRooter from "./admin/Router";
 import AppRooter from "./app/Router";
+import ScrollToTop from "./ScrollToTop";
 
 function Rooter(){
     return(
         <Router>
-            <PublicRooter/>
-            <AppRooter/>
-            <AdminRooter/>
+            <ScrollToTop>
+                <PublicRooter/>
+                <AppRooter/>
+                <AdminRooter/>
+            </ScrollToTop>
         </Router>
     )
 }
