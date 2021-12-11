73973f5261d47d3d539c09335a7ea3003576f563
Louis Lacaille <llacaille@hellojoko.com>
2021-12-11 15:37:38 +0100

    switch branch

---
+++ frontend/src/components/containers/app/glossary/SearchGlossary.js
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import Autocomplete from '@mui/material/Autocomplete';
+
+import TextField from '@mui/material/TextField';
+
+export default function SearchGlossary(props) {
+    const theme = useTheme();
+    let navigate = useNavigate();
+    const { t } = useTranslation();
+
+    const top100Films = [
+        { title: 'The Shawshank Redemption', year: 1994 },
+        { title: 'The Godfather', year: 1972 },
+        { title: 'The Godfather: Part II', year: 1974 },
+        { title: 'The Dark Knight', year: 2008 },
+        { title: '12 Angry Men', year: 1957 },
+        { title: "Schindler's List", year: 1993 },
+        { title: 'Pulp Fiction', year: 1994 },
+        {
+            title: 'The Lord of the Rings: The Return of the King',
+            year: 2003,
+        },
+        { title: 'The Good, the Bad and the Ugly', year: 1966 },
+        { title: 'Fight Club', year: 1999 },
+        {
+            title: 'The Lord of the Rings: The Fellowship of the Ring',
+            year: 2001,
+        },
+        {
+            title: 'Star Wars: Episode V - The Empire Strikes Back',
+            year: 1980,
+        },
+        { title: 'Forrest Gump', year: 1994 },
+        { title: 'Inception', year: 2010 },
+        {
+            title: 'The Lord of the Rings: The Two Towers',
+            year: 2002,
+        }
+    ]
+
+    const options = top100Films.map((option) => {
+        const firstLetter = option.title[0].toUpperCase();
+        return {
+            firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+            ...option,
+        };
+    });
+
+    return (
+        <div>
+            <Typography variant="h4" fontWeight={600}>
+                Annuaire
+            </Typography>
+
+            <Typography variant="body" fontWeight={300} fontSize={20}>
+                Rechercher parmi les 1399 employés
+            </Typography>
+
+            <Autocomplete
+                id="grouped-demo"
+                options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+                groupBy={(option) => option.firstLetter}
+                getOptionLabel={(option) => option.title}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Collègues de travail" />}
+            />
+        </div >
+    );
+}
--- frontend/src/views/app/Glossary.js
+++ frontend/src/views/app/Glossary.js
@@ -1,40 +1,41 @@
 import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import Grid from "@mui/material/Grid";
 import CustomContainer from "../../components/containers/app/CustomContainer";
-import Autocomplete from '@mui/material/Autocomplete';
+
+import TeamGlossary from "../../components/containers/app/glossary/TeamGlossary";
+import FavoriteGlossary from "../../components/containers/app/glossary/FavoriteGlossary";
+import SearchGlossary from "../../components/containers/app/glossary/SearchGlossary";
+import GridGlossary from "../../components/containers/app/glossary/GridGlossary";
 
 export default function Glossary(props) {
     const theme = useTheme();
     let navigate = useNavigate();
     const { t } = useTranslation();
 
+
     return (
         <CustomContainer>
-            <Grid wrap={"nowrap"} container direction={"row"} spacing={2} style={{ backgroundColor: theme.palette.background.paper, height: '100vh' }}>
+            <Grid wrap={"nowrap"} container direction={"row"} style={{ backgroundColor: theme.palette.background.paper, height: '100vh' }}>
+
                 <Grid item md={5} style={{ backgroundColor: '#000000' }}>
                     <Grid container direction="column" height="100%">
-                        <Grid item md={3} style={{ backgroundColor: '#FFFFFF' }}>
-                            <Typography variant="h4" fontWeight={600}>
-                                Annuaire
-                            </Typography>
-                            <Typography variant="body" fontWeight={300} fontSize={20}>
-                                Rechercher parmi les 1399 employés
-                            </Typography>
+                        <Grid item md={6} style={{ backgroundColor: '#FFFFFF' }}>
                         </Grid>
-                        <Grid item md={9}>
+                        <Grid item md={6} style={{ backgroundColor: '#1126FA' }}>
                         </Grid>
                     </Grid>
                 </Grid>
 
                 <Grid item md={7} style={{ backgroundColor: '#FAF611' }}>
-
+                    <SearchGlossary />
+                    <GridGlossary />
                 </Grid>
+
             </Grid>
-        </CustomContainer>
+        </CustomContainer >
 
     );
 }
