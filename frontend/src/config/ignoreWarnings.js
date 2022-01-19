82f4f228bbf3bcea971a321529f53dbb55326894
DELMER <d.pierrebox@gmail.com>
2022-01-19 22:52:57 +0100

    Adding prod env.

--- frontend/src/config/ignoreWarnings.js
+++ frontend/src/config/ignoreWarnings.js
@@ -3,25 +3,29 @@ function logError(...parameters) {
     let filter = parameters.find(parameter => {
         return (
             // Filter error because XXX
-            parameter.includes("Warning: Legacy context API has been detected within a strict-mode tree.") ||
-            parameter.includes("Invalid prop `maxWidth` supplied to `ForwardRef(Container)`")
+            parameter.includes("")
         );
     });
     if(!filter) error(...parameters);
 }
 console.error  = logError;
 
-
+/*
+parameter.includes("Warning: Legacy context API has been detected within a strict-mode tree.") ||
+parameter.includes("Invalid prop `maxWidth` supplied to `ForwardRef(Container)`")
+ */
 
 const warn = console.warn;
 
 function logWarning(...warnings){
     let showWarning = true;
     warnings.forEach(warning => {
-        if (warning.includes("UNSAFE_")) showWarning = false;
+        if (warning.includes("")) showWarning = false;
     });
     if(showWarning) warn(...warnings);
 }
-
+/*
+if (warning.includes("UNSAFE_")) showWarning = false;
+ */
 
 console.warn  = logWarning;
