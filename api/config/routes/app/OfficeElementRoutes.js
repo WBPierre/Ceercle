ad7818d943e1dc744706d51fb7942968bc3411ee
Louis Lacaille <llacaille@hellojoko.com>
2022-02-08 00:13:41 +0100

    tooltip that prevents from double booking

--- api/config/routes/app/OfficeElementRoutes.js
+++ api/config/routes/app/OfficeElementRoutes.js
@@ -6,6 +6,7 @@ const { isUserAuthenticated, isCeercle, isAdminUser } = require("../../../src/mi
 router.get('/floors/:id', [isUserAuthenticated, OfficeElementController.validate('getFloors')], OfficeElementController.getFloors)
 router.get('/rooms/:id/:day', [isUserAuthenticated, OfficeElementController.validate('getRooms')], OfficeElementController.getRooms)
 router.get('/desks/:id/:day', [isUserAuthenticated, OfficeElementController.validate('getDesks')], OfficeElementController.getDesks)
+router.get('/isSeatAvailable/:day/:when', [isUserAuthenticated, OfficeElementController.validate('isSeatAvailable')], OfficeElementController.isSeatAvailable);
 router.get('/:id', [isUserAuthenticated, OfficeElementController.validate('getOfficeElements')], OfficeElementController.getOfficeElements);
 router.get('/:id/full/:day', [isUserAuthenticated, OfficeElementController.validate('getOfficeElements')], OfficeElementController.getOfficeElementsWithCapacity);
 router.post('/updateOccupancy', [isAdminUser, OfficeElementController.validate('updateOccupancy')], OfficeElementController.updateOccupancy);
--- frontend/src/components/containers/app/dashboard/PlanningButton.js
+++ frontend/src/components/containers/app/dashboard/PlanningButton.js
@@ -1,6 +1,6 @@
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
-import {Avatar, ListItemIcon, ListItemText, Menu, Switch, Typography} from "@mui/material";
+import { Avatar, ListItemIcon, ListItemText, Menu, Switch, Typography } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
 import ToDefineIcon from "../../../molecules/icons/ToDefineIcon";
 import OfficeIcon from "../../../molecules/icons/OfficeIcon";
@@ -8,9 +8,25 @@ import ManWorkingIcon from "../../../molecules/icons/ManWorkingIcon";
 import AwayIcon from "../../../molecules/icons/AwayIcon";
 import OffIcon from "../../../molecules/icons/OffIcon";
 import * as React from "react";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import useAuth from "../../../context/auth/AuthHelper";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import { styled } from '@mui/material/styles';
+import Container from "@mui/material/Container";
+import OfficeService from "../../../../services/app/office.service";
+
+const HtmlTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: '#f5f5f9',
+        color: 'rgba(0, 0, 0, 0.87)',
+        maxWidth: 220,
+        fontSize: theme.typography.pxToRem(12),
+        border: '1px solid #dadde9',
+    },
+}));
 
 
 function PlanningButton(props) {
@@ -37,7 +53,7 @@ function PlanningButton(props) {
 
     const handleChange = (e) => {
         setHalf(e.target.checked);
-        if(!e.target.checked){
+        if (!e.target.checked) {
             props.modifyChoice(props.data.morning, false, 0);
         }
         props.changeHalf(e);
@@ -49,16 +65,16 @@ function PlanningButton(props) {
     }, [props.half])
 
     const getActiveBGColor = () => {
-        if(props.order === 0){
-            switch(props.data.morning){
+        if (props.order === 0) {
+            switch (props.data.morning) {
                 case 0: return "#F2F2F2";
                 case 1: return "#F7FEF5"
                 case 2: return "#F3F7FE"
                 case 3: return "#FAF6FF"
                 case 4: return "#FEFAF1"
             }
-        }else{
-            switch(props.data.afternoon){
+        } else {
+            switch (props.data.afternoon) {
                 case 0: return "#F2F2F2";
                 case 1: return "#F7FEF5"
                 case 2: return "#F3F7FE"
@@ -71,17 +87,17 @@ function PlanningButton(props) {
 
 
     const getIcon = () => {
-        if(half) {
-            if(props.order === 0){
-                switch(props.data.morning){
+        if (half) {
+            if (props.order === 0) {
+                switch (props.data.morning) {
                     case 0: return (<AwayIcon sx={{ width: 40, height: 40, display: 'none' }} />);
                     case 1: return (<OfficeIcon sx={{ width: 40, height: 40 }} />)
                     case 2: return (<ManWorkingIcon sx={{ width: 40, height: 40 }} />)
                     case 3: return (<AwayIcon sx={{ width: 40, height: 40 }} />)
                     case 4: return (<OffIcon sx={{ width: 40, height: 40 }} />)
                 }
-            }else{
-                switch(props.data.afternoon){
+            } else {
+                switch (props.data.afternoon) {
                     case 0: return (<AwayIcon sx={{ width: 40, height: 40, display: 'none' }} />);
                     case 1: return (<OfficeIcon sx={{ width: 40, height: 40 }} />)
                     case 2: return (<ManWorkingIcon sx={{ width: 40, height: 40 }} />)
@@ -89,8 +105,8 @@ function PlanningButton(props) {
                     case 4: return (<OffIcon sx={{ width: 40, height: 40 }} />)
                 }
             }
-        }else{
-            switch(props.data.morning){
+        } else {
+            switch (props.data.morning) {
                 case 0: return (<AwayIcon sx={{ width: 40, height: 40, display: 'none' }} />);
                 case 1: return (<OfficeIcon sx={{ width: 40, height: 40 }} />)
                 case 2: return (<ManWorkingIcon sx={{ width: 40, height: 40 }} />)
@@ -101,16 +117,16 @@ function PlanningButton(props) {
     }
 
     const getTextColor = () => {
-        if(props.order === 0){
-            switch(props.data.morning){
+        if (props.order === 0) {
+            switch (props.data.morning) {
                 case 0: return "#d32f2f";
                 case 1: return "#008946"
                 case 2: return "#0070C0"
                 case 3: return "#7030A0"
                 case 4: return "#FFA800"
             }
-        }else{
-            switch(props.data.afternoon){
+        } else {
+            switch (props.data.afternoon) {
                 case 0: return "#d32f2f";
                 case 1: return "#008946"
                 case 2: return "#0070C0"
@@ -121,16 +137,16 @@ function PlanningButton(props) {
     }
 
     const getBgColor = () => {
-        if(props.order === 0){
-            switch(props.data.morning){
+        if (props.order === 0) {
+            switch (props.data.morning) {
                 case 0: return "#D3D3D3";
                 case 1: return "#C3E4B6"
                 case 2: return "#DAEFFA"
                 case 3: return "#E6DCF1"
                 case 4: return "#FBE7B4"
             }
-        }else{
-            switch(props.data.afternoon){
+        } else {
+            switch (props.data.afternoon) {
                 case 0: return "#D3D3D3";
                 case 1: return "#C3E4B6"
                 case 2: return "#DAEFFA"
@@ -141,16 +157,16 @@ function PlanningButton(props) {
     }
 
     const getText = () => {
-        if(props.order === 0){
-            switch(props.data.morning){
+        if (props.order === 0) {
+            switch (props.data.morning) {
                 case 0: return props.data.past ? t('app:statuses:undeclared') : t('app:statuses:to_be_defined');
                 case 1: return t('app:statuses:office')
                 case 2: return t('app:statuses:home_working')
                 case 3: return t('app:statuses:on_the_go')
                 case 4: return t('app:statuses:off')
             }
-        }else{
-            switch(props.data.afternoon){
+        } else {
+            switch (props.data.afternoon) {
                 case 0: return props.data.past ? t('app:statuses:undeclared') : t('app:statuses:to_be_defined');
                 case 1: return t('app:statuses:office')
                 case 2: return t('app:statuses:home_working')
@@ -161,61 +177,74 @@ function PlanningButton(props) {
     }
 
     const shouldDisplayOption = (choice) => {
-        if(half){
-           if(props.order === 0){
-               if(choice === props.data.morning){
-                   return 'none';
-               }else{
-                   return 'flex';
-               }
-           } else {
-               if(choice === props.data.afternoon){
-                   return 'none';
-               }else{
-                   return 'flex';
-               }
-           }
-        }else{
-            if(choice === props.data.morning){
+        if (half) {
+            if (props.order === 0) {
+                if (choice === props.data.morning) {
+                    return 'none';
+                } else {
+                    return 'flex';
+                }
+            } else {
+                if (choice === props.data.afternoon) {
+                    return 'none';
+                } else {
+                    return 'flex';
+                }
+            }
+        } else {
+            if (choice === props.data.morning) {
                 return 'none';
-            }else{
+            } else {
                 return 'flex';
             }
         }
     }
 
 
-    return(
-        <div style={{height:'100%', width:'100%'}}>
+    const [available, setAvailable] = useState(true);
+    useEffect(() => {
+        async function isSeatAvailable() {
+            //let when = 2;
+            //if (props.half) { when = props.order }
+            //console.log(props.order)
+            const res = await OfficeService.isSeatAvailable(props.data.day, props.data.morning);
+            setAvailable(res.data.available)
+        }
+        isSeatAvailable();
+    }, [props.data.day])
+
+
+    return (
+        <div style={{ height: '100%', width: '100%' }}>
             <Button disableRipple={true} style={{
                 textTransform: 'none',
                 backgroundColor: props.data.current ? getActiveBGColor() : '',
                 cursor: !props.data.past ? 'pointer' : 'default',
-                flex:1,
-                width:'100%'
+                flex: 1,
+                width: '100%'
             }} id="basic-button"
-                    aria-controls="basic-menu"
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                    onClick={handleClick}>
+                aria-controls="basic-menu"
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}>
                 <Grid container direction={"column"}>
                     <Grid item>
-                        <Typography textAlign={"center"} fontSize={10} style={{color: props.half ? '#b4b4b4' : 'transparent'}}>{props.half ? props.order === 0 ? t('generic:morning') : t('generic:afternoon') : 'UNDEFINED'}</Typography>
+                        <Typography textAlign={"center"} fontSize={10} style={{ color: props.half ? '#b4b4b4' : 'transparent' }}>{props.half ? props.order === 0 ? t('generic:morning') : t('generic:afternoon') : 'UNDEFINED'}</Typography>
                     </Grid>
                     <Grid item style={{ display: 'flex', justifyContent: 'center' }}>
                         <Avatar sx={{ width: 65, height: 65 }}
-                                style={{
-                                    border: props.data.current ? `3px solid ${getTextColor()}` : 'none',
-                                    backgroundColor: props.data.past ? '#D3D3D3' : getBgColor()
-                                }}>
+                            style={{
+                                border: props.data.current ? `3px solid ${getTextColor()}` : 'none',
+                                backgroundColor: props.data.past ? '#D3D3D3' : getBgColor()
+                            }}>
                             {getIcon()}
                         </Avatar>
                     </Grid>
-                    <Grid item xs={12} my={2} mb={1} style={{width: '100%' }}>
+                    <Grid item xs={12} my={2} mb={1} style={{ width: '100%' }}>
                         <Typography textAlign={"center"}
-                                    style={{ color: props.data.past ? '#D3D3D3' : getTextColor(), width: '100%' }}
-                                    fontSize={props.half ? props.data.current ? 16 : 14 : props.data.current ? 18 : 16}
-                                    fontWeight={props.data.current ? 600 : 500}>{getText()}</Typography>
+                            style={{ color: props.data.past ? '#D3D3D3' : getTextColor(), width: '100%' }}
+                            fontSize={props.half ? props.data.current ? 16 : 14 : props.data.current ? 18 : 16}
+                            fontWeight={props.data.current ? 600 : 500}>{getText()}</Typography>
                     </Grid>
                 </Grid>
             </Button>
@@ -228,42 +257,63 @@ function PlanningButton(props) {
                     'aria-labelledby': 'basic-button',
                 }}
             >
-                <MenuItem key={-1} disableRipple={true} disableTouchRipple style={{backgroundColor:'transparent', cursor:'default'}}>
-                    <ListItemText primaryTypographyProps={{fontSize:12 }}>{t('generic:half')}</ListItemText>
-                    <Switch aria-label={'Switch half days'} checked={half} onChange={handleChange} value={half}/>
+                <MenuItem key={-1} disableRipple={true} disableTouchRipple style={{ backgroundColor: 'transparent', cursor: 'default' }}>
+                    <ListItemText primaryTypographyProps={{ fontSize: 12 }}>{t('generic:half')}</ListItemText>
+                    <Switch aria-label={'Switch half days'} checked={half} onChange={handleChange} value={half} />
                 </MenuItem>
-                <MenuItem key={0} style={{display:shouldDisplayOption(0)}} onClick={() => modifyChoice(0)}>
+
+                <MenuItem key={0} style={{ display: shouldDisplayOption(0) }} onClick={() => modifyChoice(0)}>
                     <ListItemIcon>
                         <ToDefineIcon />
                     </ListItemIcon>
                     <ListItemText>{t('app:statuses:to_be_defined')}</ListItemText>
                 </MenuItem>
-                <MenuItem key={1} style={{display:shouldDisplayOption(1)}}  onClick={() => modifyChoice(1)}>
-                    <ListItemIcon>
-                        <OfficeIcon />
-                    </ListItemIcon>
-                    <ListItemText>{t('app:statuses:office')}</ListItemText>
-                </MenuItem>
-                <MenuItem key={2} style={{display:shouldDisplayOption(2)}} onClick={() => modifyChoice(2)}>
+
+                <HtmlTooltip
+                    title={available ? "" :
+                        <Container maxWidth={false} disableGutters={true} >
+                            <Grid container direction="column">
+                                <Grid item>
+                                    <Typography fontWeight="bold" fontSize={14} color="#6A6968"> {t('app:dashboard:desk.no_remaining_seat')}</Typography>
+                                </Grid>
+                            </Grid>
+                        </Container>
+                    }
+                    followCursor
+                >
+                    <div>
+                        <MenuItem key={1} style={{ display: shouldDisplayOption(1) }} onClick={() => modifyChoice(1)} disabled={!available}>
+                            <ListItemIcon>
+                                <OfficeIcon />
+                            </ListItemIcon>
+                            <ListItemText>{t('app:statuses:office')}</ListItemText>
+                        </MenuItem>
+                    </div>
+                </HtmlTooltip>
+
+                <MenuItem key={2} style={{ display: shouldDisplayOption(2) }} onClick={() => modifyChoice(2)}>
                     <ListItemIcon>
                         <ManWorkingIcon />
                     </ListItemIcon>
                     <ListItemText>{t('app:statuses:home_working')}</ListItemText>
                 </MenuItem>
-                <MenuItem key={3} style={{display:shouldDisplayOption(3)}} onClick={() => modifyChoice(3)}>
+
+                <MenuItem key={3} style={{ display: shouldDisplayOption(3) }} onClick={() => modifyChoice(3)}>
                     <ListItemIcon>
                         <AwayIcon />
                     </ListItemIcon>
                     <ListItemText>{t('app:statuses:on_the_go')}</ListItemText>
                 </MenuItem>
-                <MenuItem key={4} style={{display:shouldDisplayOption(4)}} onClick={() => modifyChoice(4)}>
+
+                <MenuItem key={4} style={{ display: shouldDisplayOption(4) }} onClick={() => modifyChoice(4)}>
                     <ListItemIcon>
                         <OffIcon />
                     </ListItemIcon>
                     <ListItemText>{t('app:statuses:off')}</ListItemText>
                 </MenuItem>
-            </Menu>
-        </div>
+
+            </Menu >
+        </div >
     )
 }
 
