1ccdbd6b70e8328d26fc53f613906c86627f7fd8
DELMER <d.pierrebox@gmail.com>
2022-03-13 17:21:07 +0100

    Fixing int request

--- api/src/controllers/admin/TestController.js
+++ api/src/controllers/admin/TestController.js
@@ -8,19 +8,19 @@ exports.testSlack = async function(req, res, next) {
     console.log(hasIntegration);
     if(hasIntegration.length !== 0) {
         console.log(hasIntegration[0].token);
-        let getSlackUserList = await axios.post('https://slack.com/api/users.list', {token: hasIntegration[0].token});
+        let getSlackUserList = await axios.post('https://slack.com/api/users.list', new URLSearchParams({token: hasIntegration[0].token}));
         console.log(getSlackUserList);
         let slackUserList = getSlackUserList.data.members;
         const users = await ceercle.getUsers({where: {active: true, isDeleted: false}});
         for (let j = 0; j < users.length; j++) {
             for(let k = 0; k < slackUserList.length; k++){
                 if(slackUserList[k].profile.email === users[j].email){
-                    await axios.post("https://slack.com/api/chat.postMessage", {
+                    await axios.post("https://slack.com/api/chat.postMessage", new URLSearchParams({
                         token: hasIntegration[0].token,
                         channel: slackUserList[k].id,
                         text: users[j].lang === "fr" ? "N'oubliez pas de mettre à jour votre déclaration pour la semaine prochaine sur https://app.ceercle.io !" : "Don't forget to update your status for next week on https://app.ceercle.io !",
                         as_user: true
-                    })
+                    }))
                     break;
                 }
             }
